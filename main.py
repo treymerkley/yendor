ca415abece26e7152870df2bd09eed6f45c9647e
Billy Wade <billy.wade@protonmail.ch>
2019-01-17 22:08:16 -0600

    fixed readme, refactoring

--- main.py
+++ main.py
@@ -1,64 +1,16 @@
 
 #! /usr/bin/python
-
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QLabel
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QWidget
 
-class App(QMainWindow):
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Yendor')
+    w.show()
+    # label = QLabel('Hello World!')
+    # label.show()
 
-    def __init__(self):
-        super().__init__()
-        self.title = 'Yendor DM Assistant'
-        self.left = 0
-        self.top = 0
-        self.width = 300
-        self.height = 200
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-
-        self.table_widget = MyTableWidget(self)
-        self.setCentralWidget(self.table_widget)
-
-        self.show()
-
-class MyTableWidget(QWidget):
-
-    def __init__(self, parent):
-        super(QWidget, self).__init__(parent)
-        self.layout = QVBoxLayout(self)
-
-        # Initialze tab screen
-        self.sabs = QTabWidget()
-        self.tab1 = QWidget()
-        self.tab2 = QWidget()
-        self.tabs.resize(300,200)
-
-        # Add tabs
-        self.tabs.addTab(self.tab1, "Tab 1")
-        self.tabs.addTab(self.tab2, "Tab 2")
-
-        # Create first tab
-        self.tab1.layout = QVBoxLayout(self)
-        self.pushButton1 = QPushButton("PyQt5 button")
-        self.tab1.layout.addWidget(self.pushButton1)
-        self.tab1.setLayout(self.tab1.layout)
-
-        # Add tabs to widget
-        self.layout.addWidget(self.tabs)
-        self.setLayout(self.layout)
-
-        @pyqtSlot()
-        def on_click(self):
-            print("\n")
-            for currentQTableWidgetItem in self.tableWidget.selectedItems():
-                print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
- 
-
-        # if __name__ == "__main__":
-        #     app = QApplication(sys.argv)
-        #     ex = App()
-        #     sys.exit(app.exec_())
-
-        App.exec_()
+    sys.exit(app.exec_())
