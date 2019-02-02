b5b6c7b547f8fbd351acafe99c5e62b9332e6e46
Billy Wade <billy.wade@protonmail.ch>
2019-02-02 09:32:36 -0600

    refactoring

--- yendor.py
+++ yendor.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import sys
 from yendor import core
 
 core.main()
--- yendor/gui/npc.py
+++ yendor/gui/npc.py
@@ -1,39 +1,43 @@
-from PyQt5.QtWidgets import (QHBoxLayout, QComboBox, QCheckBox, QLabel)
+from PyQt5.QtWidgets import (QHBoxLayout, QVBoxLayout, QComboBox, QCheckBox, QLabel)
 def tab():
-    tab.layout = QHBoxLayout()
+    tab.layout = QVBoxLayout()
 
     speciesComboBoxLabel = QLabel()
-    speciesComboBoxLabel.setText("Species")
+    speciesComboBoxLabel.setText("Species: ")
     speciesComboBox = QComboBox()
     listOfSpecies = ('Dragonborn', 'Dwarf', 'Eldarin', 'Elf',
                      'Halfling', 'Human', 'Tiefling', 'Goblin')
     speciesComboBox.addItems(listOfSpecies)
 
     levelComboBoxLabel = QLabel()
-    levelComboBoxLabel.setText("Level")
+    levelComboBoxLabel.setText("Level: ")
     levelComboBox = QComboBox()
     for i in range(1, 31):
         levelComboBox.addItem(str(i))
         i += 1
 
     classesComboBoxLabel = QLabel()
-    classesComboBoxLabel.setText("Class")
+    classesComboBoxLabel.setText("Class: ")
     classesComboBox = QComboBox()
     listOfClasses = ('Fighter', 'Rogue', 'Mage', 'Cleric')
     classesComboBox.addItems(listOfClasses)
 
     elderCheckBoxLabel = QLabel()
-    elderCheckBoxLabel.setText("Elder")
+    elderCheckBoxLabel.setText("Elder: ")
     elderCheckBox = QCheckBox()
 
+    controls = QHBoxLayout()
+
     # adding widgets to layout
-    tab.layout.addWidget(levelComboBoxLabel)
-    tab.layout.addWidget(levelComboBox)
-    tab.layout.addWidget(speciesComboBoxLabel)
-    tab.layout.addWidget(speciesComboBox)
-    tab.layout.addWidget(classesComboBoxLabel)
-    tab.layout.addWidget(classesComboBox)
-    tab.layout.addWidget(elderCheckBoxLabel)
-    tab.layout.addWidget(elderCheckBox)
+    controls.addWidget(levelComboBoxLabel)
+    controls.addWidget(levelComboBox)
+    controls.addWidget(speciesComboBoxLabel)
+    controls.addWidget(speciesComboBox)
+    controls.addWidget(classesComboBoxLabel)
+    controls.addWidget(classesComboBox)
+    controls.addWidget(elderCheckBoxLabel)
+    controls.addWidget(elderCheckBox)
+
+    tab.layout.addLayout(controls)
 
     return tab.layout
