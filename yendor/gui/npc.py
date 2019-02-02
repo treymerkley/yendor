dd0719fa9307c54b7209cc4fd4cdb2957ddfa4b8
Billy Wade <billy.wade@protonmail.ch>
2019-02-02 09:52:08 -0600

    enforce python3

--- yendor/gui/npc.py
+++ yendor/gui/npc.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 from PyQt5.QtWidgets import (QHBoxLayout, QVBoxLayout,
                              QComboBox, QCheckBox, QLabel)
 def tab():
--- yendor/gui/places.py
+++ yendor/gui/places.py
@@ -1,7 +1,9 @@
+#! /usr/bin/python3
 from PyQt5.QtWidgets import QHBoxLayout
 def tab():
     tab.layout = QHBoxLayout()
 
+    # probably village, city, cave, ruins
     # code goes here
 
     return tab.layout
