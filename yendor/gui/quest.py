f67c7271286be5207226cdde3ce464f27e6fe727
Billy Wade <billy.wade@protonmail.ch>
2019-02-07 09:00:28 -0600

    added ui for quest page

--- yendor/gui/quest.py
+++ yendor/gui/quest.py
@@ -1,7 +1,58 @@
-from PyQt5.QtWidgets import QHBoxLayout
+#! /usr/bin/python3
+from PyQt5.QtWidgets import (QHBoxLayout, QVBoxLayout, QComboBox,
+                             QLabel, QPushButton, QTextEdit)
+
 def tab():
-    tab.layout = QHBoxLayout()
+    # Initializing the complete layout
+    tab.layout = QVBoxLayout()
 
-    # code goes here
+    # The list of plots to choose from. Change the values in the listOfPlot
+    # list to get different results on the page.
+    plotComboBoxLabel = QLabel()
+    plotComboBoxLabel.setText("Basic Plot:")
+    plotComboBox = QComboBox()
+    listOfPlot = ('Go kill a monster', 'Go kill a noble', 'Go kill a god',
+                  'Bad hombres moved in', 'Bad hombres at the top',
+                  'Escort quest', 'Go get a thing', 'Go get some cash',
+                  'Go investigate the spoopy place',
+                  'Go investigate the ostensibly normal place')
+    plotComboBox.addItems(listOfPlot)
 
+    # This sets up the level scaling for the resulting character. The current
+    # cap is at 30, but the algorithm should continue to function to any level.
+    levelComboBoxLabel = QLabel()
+    levelComboBoxLabel.setText("Level:")
+    levelComboBox = QComboBox()
+    for i in range(1, 31):
+        levelComboBox.addItem(str(i))
+        i += 1
+
+    # These are the buttons for saving and generating new quests.
+    generateButton = QPushButton("Generate")
+    saveButton = QPushButton("Save")
+
+    bottomButtonsLayout = QHBoxLayout()
+    bottomButtonsLayout.addStretch(1)
+    bottomButtonsLayout.addWidget(generateButton)
+    bottomButtonsLayout.addWidget(saveButton)
+
+    # This builds the textbox that you see the resulting character in
+    mainTextBox = QTextEdit()
+    textBoxLayout = QHBoxLayout()
+    textBoxLayout.addWidget(mainTextBox)
+
+    # This creates the layout for the controls. Any new fields should
+    # follow this same general convention.
+    controls = QHBoxLayout()
+    controls.addWidget(levelComboBoxLabel)
+    controls.addWidget(levelComboBox)
+    controls.addWidget(plotComboBoxLabel)
+    controls.addWidget(plotComboBox)
+
+    # Adds all of the disparate groups of controls to the total layout
+    tab.layout.addLayout(controls)
+    tab.layout.addLayout(textBoxLayout)
+    tab.layout.addLayout(bottomButtonsLayout)
+
+    # sends the layout to core.py
     return tab.layout
